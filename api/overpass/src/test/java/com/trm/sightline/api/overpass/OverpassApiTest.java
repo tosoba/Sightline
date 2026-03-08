930be803061f00f96835344795350d93b724976b
tosoba <therealmerengue@gmail.com>
2026-03-08 12:28:46 +0100

    Add more tag based queries in OverpassApiTest

--- api/overpass/src/test/java/com/trm/sightline/api/overpass/OverpassApiTest.java
+++ api/overpass/src/test/java/com/trm/sightline/api/overpass/OverpassApiTest.java
@@ -29,7 +29,7 @@ public class OverpassApiTest {
                 new NodeQuery.Builder()
                     .timeout(25)
                     .tag("amenity", "post_box")
-                    .around(52.5, 13.4, 500.0f)
+                    .around(52.237049, 21.017532, 1000f)
                     .build()
                     .toQuery())
             .execute()
@@ -45,7 +45,7 @@ public class OverpassApiTest {
                 new NodeQuery.Builder()
                     .timeout(25)
                     .is("amenity")
-                    .around(52.5, 13.4, 500.0f)
+                    .around(52.237049, 21.017532, 1000f)
                     .build()
                     .toQuery())
             .execute()
@@ -62,7 +62,68 @@ public class OverpassApiTest {
                 "amenity",
                 "bar|biergarten|cafe|fast_food|food_court|ice_cream|pub|restaurant",
                 Filter.LIKE)
-            .around(52.5, 13.4, 500.0f)
+            .around(52.237049, 21.017532, 1000f)
+            .build()
+            .toQuery();
+    System.out.println(query);
+    System.out.println(overpassApi.ask(query).execute().body().toString());
+  }
+
+  @Test
+  public void tourismAccommodation() throws IOException {
+    final String query =
+        new NodeQuery.Builder()
+            .timeout(25)
+            .tag(
+                "tourism",
+                "alpine_hut|apartment|chalet|guest_house|hostel|hotel|motel",
+                Filter.LIKE)
+            .around(52.237049, 21.017532, 1000f)
+            .build()
+            .toQuery();
+    System.out.println(query);
+    System.out.println(overpassApi.ask(query).execute().body().toString());
+  }
+
+  @Test
+  public void tourismPlacesToSee() throws IOException {
+    final String query =
+        new NodeQuery.Builder()
+            .timeout(25)
+            .tag(
+                "tourism",
+                "artwork|attraction|aquarium|gallery|museum|theme_park|viewpoint|zoo",
+                Filter.LIKE)
+            .around(52.237049, 21.017532, 1000f)
+            .build()
+            .toQuery();
+    System.out.println(query);
+    System.out.println(overpassApi.ask(query).execute().body().toString());
+  }
+
+  @Test
+  public void leisure() throws IOException {
+    final String query =
+        new NodeQuery.Builder()
+            .timeout(25)
+            .tag(
+                "leisure",
+                "adult_gaming_centre|amusement_arcade|beach_resort|bowling_alley|dance|disc_golf_course|dog_park|escape_game|fishing|fitness_centre|fitness_station|garden|golf_course|hackerspace|high_ropes_course|horse_riding|ice_rink|marina|miniature_gold|nature_reserve|park|pitch|playground|resort|sauna|sports_centre|sports_hall|stadium|sunbathing|swimming_pool|track|trampoline_park",
+                Filter.LIKE)
+            .around(52.237049, 21.017532, 1000f)
+            .build()
+            .toQuery();
+    System.out.println(query);
+    System.out.println(overpassApi.ask(query).execute().body().toString());
+  }
+
+  @Test
+  public void generalStores() throws IOException {
+    final String query =
+        new NodeQuery.Builder()
+            .timeout(25)
+            .tag("shop", "department_store|general|kiosk|mall|supermarket|wholesale", Filter.LIKE)
+            .around(52.237049, 21.017532, 1000f)
             .build()
             .toQuery();
     System.out.println(query);
