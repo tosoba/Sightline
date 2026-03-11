214d884231c02163b656d9fc12538dcbd37505ce
merengue <therealmerengue@proton.me>
2026-03-11 17:28:03 +0100

    Add markers with clustering to MapPreview

--- feature/map/build.gradle.kts
+++ feature/map/build.gradle.kts
@@ -40,9 +40,10 @@ dependencies {
   implementation(platform(libs.androidx.compose.bom))
   implementation(libs.androidx.compose.ui)
   implementation(libs.androidx.compose.ui.graphics)
-  implementation(libs.androidx.compose.ui.tooling.preview)
-  implementation(libs.androidx.compose.material3)
   debugImplementation(libs.androidx.compose.ui.tooling)
+  implementation(libs.androidx.compose.ui.tooling.preview)
+  implementation(libs.androidx.compose.material.icons.extended)
+  implementation(libs.androidx.compose.material3)
 
   implementation(libs.maplibre.compose)
 
--- feature/map/src/main/java/com/trm/sightline/feature/map/MapPreview.kt
+++ feature/map/src/main/java/com/trm/sightline/feature/map/MapPreview.kt
@@ -1,13 +1,41 @@
 package com.trm.sightline.feature.map
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Man
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import com.trm.sightline.core.model.Marker
+import kotlinx.serialization.json.JsonPrimitive
+import org.maplibre.compose.expressions.dsl.asNumber
+import org.maplibre.compose.expressions.dsl.asString
+import org.maplibre.compose.expressions.dsl.const
+import org.maplibre.compose.expressions.dsl.convertToNumber
+import org.maplibre.compose.expressions.dsl.feature
+import org.maplibre.compose.expressions.dsl.image
+import org.maplibre.compose.expressions.dsl.not
+import org.maplibre.compose.expressions.dsl.plus
+import org.maplibre.compose.expressions.dsl.step
+import org.maplibre.compose.layers.CircleLayer
+import org.maplibre.compose.layers.SymbolLayer
 import org.maplibre.compose.map.MapOptions
 import org.maplibre.compose.map.MaplibreMap
 import org.maplibre.compose.map.OrnamentOptions
+import org.maplibre.compose.sources.GeoJsonData
+import org.maplibre.compose.sources.GeoJsonOptions
+import org.maplibre.compose.sources.rememberGeoJsonSource
 import org.maplibre.compose.style.BaseStyle
+import org.maplibre.compose.util.ClickResult
+import org.maplibre.spatialk.geojson.Feature
+import org.maplibre.spatialk.geojson.FeatureCollection
+import org.maplibre.spatialk.geojson.Point
+import org.maplibre.spatialk.geojson.Position
 
 @Composable
 fun MapPreview(markers: List<Marker>, modifier: Modifier = Modifier) {
@@ -18,7 +46,75 @@ fun MapPreview(markers: List<Marker>, modifier: Modifier = Modifier) {
         "https://tiles.openfreemap.org/styles/${if (isSystemInDarkTheme()) OpenFreeMapStyle.Dark.name.lowercase() else OpenFreeMapStyle.Liberty.name.lowercase()}"
       ),
     options = MapOptions(ornamentOptions = OrnamentOptions.AllDisabled),
-  ) {}
+  ) {
+    val source =
+      rememberGeoJsonSource(
+        GeoJsonData.Features(
+          remember(markers) {
+            FeatureCollection(
+              markers.map {
+                Feature(
+                  id = JsonPrimitive(it.id.toString()),
+                  geometry = Point(Position(longitude = it.longitude, latitude = it.latitude)),
+                  properties = Unit,
+                )
+              }
+            )
+          }
+        ),
+        GeoJsonOptions(
+          cluster = true,
+          clusterRadius = 32,
+          clusterMaxZoom = 16,
+          clusterProperties =
+            mapOf(
+              "total_range" to
+                GeoJsonOptions.ClusterPropertyAggregator(
+                  mapper = feature["current_range_meters"].convertToNumber(),
+                  reducer =
+                    feature.accumulated().asNumber() + feature["total_range"].convertToNumber(),
+                )
+            ),
+        ),
+      )
+
+    CircleLayer(
+      id = "clustered-markers",
+      source = source,
+      filter = feature.has("point_count"),
+      color = const(Color.Green),
+      opacity = const(0.5f),
+      radius =
+        step(
+          input = feature["point_count"].asNumber(),
+          fallback = const(15.dp),
+          25 to const(20.dp),
+          100 to const(30.dp),
+          500 to const(40.dp),
+          1000 to const(50.dp),
+          5000 to const(60.dp),
+        ),
+      onClick = { _ -> ClickResult.Consume },
+    )
+
+    SymbolLayer(
+      id = "clustered-bikes-count",
+      source = source,
+      filter = feature.has("point_count"),
+      textField = feature["point_count_abbreviated"].asString(),
+      textFont = const(listOf("Noto Sans Regular")),
+      textColor = const(MaterialTheme.colorScheme.onBackground),
+    )
+
+    SymbolLayer(
+      id = "unclustered-markers",
+      source = source,
+      filter = !feature.has("point_count"),
+      iconImage =
+        image(value = rememberVectorPainter(Icons.Filled.Man), size = DpSize(14.dp, 14.dp)),
+      iconAllowOverlap = const(true),
+    )
+  }
 }
 
 private enum class OpenFreeMapStyle {
