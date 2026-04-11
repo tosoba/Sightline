a91b366b32a5ff4778a8573a4cafdfeefd65d2c2
tosoba <therealmerengue@gmail.com>
2026-04-11 13:20:38 +0200

    Remove empty consumerProguardFiles

--- core/ar/build.gradle.kts
+++ core/ar/build.gradle.kts
@@ -12,7 +12,6 @@ android {
     minSdk = libs.versions.minSdk.get().toInt()
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    consumerProguardFiles("consumer-rules.pro")
 
     externalNativeBuild {
       cmake {
